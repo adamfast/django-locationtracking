44ea3cfefa5965b1a5c90b2ff4d239d8b24e8e2d
Adam Fast <adamfast@gmail.com>
2008-09-19 02:33:12 +0000

    Initial checkin, has been split fully from adamfast.com codebase

---
+++ instamapper.py
@@ -0,0 +1,35 @@
+import csv
+import datetime
+from dateutil import tz
+
+class InstaMapperPosition():
+    """A class to take a single InstaMapper string and return in a more usable format."""
+
+    def make_timestamp(self):
+        self.timestamp = datetime.datetime.utcfromtimestamp(self.timestamp_raw)
+        self.timestamp = self.timestamp.replace(tzinfo=tz.gettz('UTC'))
+
+    def make_miles_per_hour(self):
+        self.speed_miles_per_hour = self.speed_meters_per_second * 2.23693629
+
+    def __init__(self, value):
+        reader = csv.reader([value,], quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            if row:
+                if not row[0] == 'InstaMapper API v1.00':
+                    self.device_key = row[0]
+                    self.device_label = row[1]
+                    self.timestamp_raw = float(row[2])
+                    self.make_timestamp()
+                    self.latitude = row[3]
+                    self.longitude = row[4]
+                    self.altitude_meters = row[5]
+                    self.speed_meters_per_second = float(row[6])
+                    self.make_miles_per_hour()
+                    self.heading = row[7]
+
+def _test():
+    pass
+
+if __name__ == '__main__':
+    _test()
---
+++ templatetags/tracking.py
@@ -0,0 +1,34 @@
+import re
+from django import template
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from locationtracking.models import PositionReport
+
+register = template.Library()
+
+class LatestPositionReportNode(template.Node):
+    def __init__(self, user, var_name):
+        user_obj = get_object_or_404(User, username__exact=user)
+        self.user = user_obj
+        self.var_name = var_name
+
+    def render(self, context):
+        report = PositionReport.objects.filter(user__exact=self.user).order_by('-timestamp_received')[0]
+        context[self.var_name] = report
+        return ''
+
+def get_latest_position(parser, token):
+    # This version uses a regular expression to parse tag contents.
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires arguments" % token.contents.split()[0]
+
+    m = re.search(r'(.*?) as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError, "%r tag had invalid arguments" % tag_name
+    user, var_name = m.groups()
+    return LatestPositionReportNode(user, var_name)
+register.tag('get_latest_position', get_latest_position)
