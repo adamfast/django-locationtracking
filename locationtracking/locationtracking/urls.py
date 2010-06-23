5ad1cde8df6c081fe45ad727fa5d0325c3aa1592
Adam Fast <adam@adamfast.com>
2010-06-22 21:20:43 -0500

    Revert "Nest another level deep to follow conventions."
    This reverts commit b7bc48c4b77e6a779337aeee033fcd50d83ca7e4.

--- locationtracking/locationtracking/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'(?P<start_date>[-\w]+)/(?P<end_date>[-\w]+)/$', 'locationtracking.views.track'),
-    (r'(?P<start_date>[-\w]+)/$', 'locationtracking.views.track'),
-    (r'$', 'locationtracking.views.track'),
-)
