0a05156d7808adb3028adf8d010fe09fa558174d
Adam Fast <adam@adamfast.com>
2010-08-08 10:38:28 -0500

    Ensure attribs exist before doing math on them. Also, refactor how retrieve_positions is called to be simpler when a report hasn't come in from that particular source.

--- locationtracking/bin/import_aprs.py
+++ locationtracking/bin/import_aprs.py
@@ -18,11 +18,17 @@ def retrieve_positions(callsign, user, since):
         report.point = Point(position.longitude, position.latitude)
         report.heading = position.course
         # APRSworld stores speed in kph and altitude in m, but all the other APRS sites store in imperial units
-        report.speed = position.speed * 0.621371192
-        report.altitude = position.altitude * 3.2808399
+        if position.speed:
+            report.speed = position.speed * 0.621371192
+        if position.altitude:
+            report.altitude = position.altitude * 3.2808399
         report.save()
 
 if __name__ == '__main__':
+    """You should copy this code into your own APRS importer somewhere - then the
+    callsign / user / positionreportsource can be changed if you want, and future
+    changes to this file won't overwrite your modifications."""
+
     import datetime
     from django.contrib.auth.models import User
 
@@ -31,8 +37,6 @@ if __name__ == '__main__':
 
     try:
         last_position = PositionReport.objects.filter(user=user, source=PositionReportSource.objects.get_or_create(name='APRSWorld')[0]).order_by('-timestamp_received')[0]
+        retrieve_positions(callsign, user, last_position.timestamp_received) # ask for anything after the last one we have
     except IndexError:
-        last_position = object
-        last_position.timestamp_received = datetime.datetime(2001, 1, 1) # way in the past if nothing is found
-
-    retrieve_positions(callsign, user, last_position.timestamp_received) # ask for anything after the last one we have
+        retrieve_positions(callsign, user, datetime.datetime(2001, 1, 1)) # way in the past if nothing for that source was found
